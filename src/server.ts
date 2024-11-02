831dbc20f74dcb9e6859d855a71ebf71a5b8ded2
Aak172003 <prajapatiaakash364@gmail.com>
2024-11-02 13:17:12 +0530

    setup eslint

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,10 @@ console.log("Welcome to Fullstack Engineering Course");
 function Welcome(name: string) {
     console.log(`Welcoem + ${name}`);
     console.log("Welcome " + name);
+
+    const user = { name: "Aakash" };
+
+    console.log("user ====== ", user.name);
 }
 
 Welcome("Aakash");
