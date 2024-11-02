45f828489f31bd5b2766e814b41ee5d9c43303d9
Aak172003 <prajapatiaakash364@gmail.com>
2024-11-02 12:13:23 +0530

    add prettier setup

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 console.log("Welcome to Fullstack Engineering Course");
 
 function Welcome(name: string) {
-  console.log(`Welcoem + ${name}`);
+    console.log(`Welcoem + ${name}`);
+    console.log("Welcome " + name);
 }
 
 Welcome("Aakash");
