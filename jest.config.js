30c19357d377a6efd73462ee1da621cf5e4e8f23
Aak172003 <prajapatiaakash364@gmail.com>
2024-11-27 08:28:49 +0530

    new-strategy-to-read-private-key-while-generating-accesstokne

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,8 @@ module.exports = {
 
     // becuase of verbose true , test cases will show in proper structured way
     verbose: true,
+
+    // below three setup while setup test coverage
     collectCoverage: true,
     coverageProvider: "v8",
     collectCoverageFrom: ["src/**/*.ts", "!tests/**", "!**/node_modules/**"],
